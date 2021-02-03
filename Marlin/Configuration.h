35618bb47b9b588b9fa1c7b808df56bd9f276c75
Daniele INeDiA <inedia@gmail.com>
2021-02-03 19:05:07 +0100

    aumentato Z max, diminuita matrice bed leveling

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1099,7 +1099,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 185
+#define Z_MAX_POS 200
 
 /**
  * Software Endstops
@@ -1249,7 +1249,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 4
+  #define GRID_MAX_POINTS_X 3
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
